668b664e56962a8ba543a4d971e236cebbb210c8
hadala-p <phadala24@gmail.com>
2024-01-03 15:54:26 +0100

    Added book data model

--- src/main/java/pl/library/libraryonline/LibraryOnlineApplication.java
+++ src/main/java/pl/library/libraryonline/LibraryOnlineApplication.java
@@ -2,22 +2,12 @@ package pl.library.libraryonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import pl.library.libraryonline.Model.Book;
-import pl.library.libraryonline.Repository.BookRepository;
 
 @SpringBootApplication
 public class LibraryOnlineApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(LibraryOnlineApplication.class, args);
-        BookRepository bookRepository = context.getBean(BookRepository.class);
-        Book book1 = new Book("Kryminał", "W pustyni i w puszczy", "Sienkiewicz", "Nowa era", 2000,
-                654, "malowniczy świat", "img");
-        bookRepository.save(book1);
-        Book book2 = new Book("Dramat", "Potop", "Sienkiewicz", "Helium", 1999,
-                765, "Super dramat", "img");
-        bookRepository.save(book2);
+        SpringApplication.run(LibraryOnlineApplication.class, args);
 
     }
 
--- src/main/java/pl/library/libraryonline/Repository/BookRepository.java
+++ src/main/java/pl/library/libraryonline/Repository/BookRepository.java
@@ -1,7 +1,7 @@
 package pl.library.libraryonline.Repository;
 
 import org.springframework.data.repository.CrudRepository;
-import pl.library.libraryonline.Model.Book;
+import pl.library.libraryonline.Domain.book.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
